94b8726d82aaace359bb4761814dbf503e1d5a1e
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-03-04 22:07:00 +0100

    periodic multidomain works

--- nanomesh/generator.py
+++ nanomesh/generator.py
@@ -27,6 +27,7 @@ class Generator(object):
         # Long crystal axis, usually a = sqrt(c)
         self.a = a
         self.c = c
+
         # Pore radius
         self.r = r
 
@@ -55,6 +56,34 @@ class Generator(object):
             if self.check_pore(r[2], r[1]) or self.check_pore(r[0], r[1] + self.a / 4):
                 result[ix, iy, iz] = bin_val[1]
         return result.astype('uint8')
+
+    def generate_vect(self, sizes: List[int], resolution: List[float], transform=None, bin_val = [0., 1.]):
+        """
+        Generate a volume image of the structure
+        Args:
+            sizes: The size (3 integers) of the resulting volume in voxels
+            resolution: The resolution of each voxel (xray images are at 10nm or 20nm)
+            transform: Optional 3D transformation matrix to map from the coordinate system of the
+             structure to the coordinate system of the volume. It should have determinant of +-1
+             for the resolution to remain correct.
+        Returns: A ndarray of size sizes filled with either 1 (air) or 0 (silicon)
+        """
+        result = bin_val[0] * np.ones(sizes)
+
+        # Invert all the transform
+        if transform is not None:
+            transform = np.linalg.inv(transform)
+
+        r = np.array(list(np.ndindex(result.shape)))
+        r = np.array(resolution) * r
+        r = r @ transform.T
+
+        shift = np.array([0,  + self.a / 4])
+        cond = self.check_pore_vect(r[:,[2,1]]) + self.check_pore_vect(r[:,[0,1]]+shift)
+        cond = cond.reshape(*sizes)
+        result[cond] = bin_val[1]
+
+        return result #.astype('uint8')
 
     def check_pore(self, xz: float, y: float) -> bool:
         """
@@ -89,3 +118,42 @@ class Generator(object):
             xzrh = xzr - self.c / 2
             yrh = yr - self.a / 2
             return yrh * yrh + xzrh * xzrh < r2
+
+    def check_pore_vect(self, data) -> bool:
+        """
+        Helper function to determine whether a 2D coordinate is inside a pore.
+        Args:
+            xz: The coordinate along the axis of length c (COPS coordinates X and Z)
+            y: The coordinate along the axis of length a (COPS coordinate Y)
+
+        Returns: Whether the given coordinate is within an air pore.
+        """
+
+        # Check for a pore in the xy or zy plane.
+        # This places pores at the corners
+        # xz,y = (0,0), (0,a), (c,0), (c,a)
+        # and the centre (xz,y) = (c/2,a/2)
+
+        xz = data[:,0]
+        y = data[:,1]
+
+        # Compute the coordinate within the regular unit cell.
+        xzr = xz % self.c
+        yr = y % self.a
+
+        r2 = self.r * self.r
+
+        xzr2 = xzr * xzr
+        yr2 = yr * yr
+
+        xzrc2 = (self.c - xzr) * (self.c - xzr)
+        yra2 = (self.a - yr) * (self.a - yr)
+    
+
+        cond =  ((yr2 + xzr2) < r2) + (yra2 + xzr2 < r2) + (yr2 + xzrc2 < r2) + (yra2 + xzrc2 < r2)
+
+        xzrh = xzr - self.c / 2
+        yrh = yr - self.a / 2
+        cond = cond +  (yrh * yrh + xzrh * xzrh < r2)
+        
+        return cond

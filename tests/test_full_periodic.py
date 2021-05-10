fd7708eb880c158eecdc5280724a92fc2da1292e
Stef Smeets <s.smeets@esciencecenter.nl>
2021-05-10 11:33:49 +0200

    Add test for full periodic 1 domain mesh generation

---
+++ tests/test_full_periodic.py
@@ -0,0 +1,41 @@
+import pickle
+from pathlib import Path
+
+import pygalmesh
+
+from nanomesh.structures import XDIM, YDIM, ZDIM, Pore3D
+
+
+def test_full_periodic_1domain():
+    """Test whether Pore3D gives the expected result with pygalmesh."""
+    mesh = pygalmesh.generate_periodic_mesh(
+        Pore3D(), [0, 0, 0, XDIM, YDIM, ZDIM],
+        max_cell_circumradius=0.025,
+        min_facet_angle=30,
+        max_radius_surface_delaunay_ball=0.025,
+        max_facet_distance=0.025,
+        max_circumradius_edge_ratio=2,
+        number_of_copies_in_output=1,
+        make_periodic=True,
+        exude=False,
+        perturb=False,
+        odt=False,
+        lloyd=False,
+        verbose=True)
+
+    expected_fn = Path(__file__).parent / 'full_periodic_1domain.pickle'
+
+    if expected_fn.exists():
+        with open(expected_fn, 'rb') as f:
+            expected_mesh = pickle.load(f)
+    else:
+        with open(expected_fn, 'wb') as f:
+            pickle.dump(mesh, f)
+
+        RuntimeError(f'Wrote expected mesh to {expected_fn.absolute()}')
+
+    assert expected_mesh == mesh
+
+
+def test_full_periodic_2domain():
+    pass

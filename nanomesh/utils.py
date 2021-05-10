7ccd50ed340555aab88ac95b9099e01f0e6d367c
Stef Smeets <s.smeets@esciencecenter.nl>
2021-05-10 15:54:21 +0200

    Test and update non-periodic notebook

--- nanomesh/utils.py
+++ nanomesh/utils.py
@@ -83,7 +83,7 @@ def show_volume(data):
     return itkw.view(data)
 
 
-def generate_mesh_from_binary_image(img, h=[1.] * 3, **kwargs):
+def generate_mesh_from_binary_image(img, h=(1.0, 1.0, 1.0), **kwargs):
     img_array = sitk.GetArrayFromImage(img)
     mesh = pygalmesh.generate_from_array(img_array, h, **kwargs)
     return mesh

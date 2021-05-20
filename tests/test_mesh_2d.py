7ee54f44262d0614c605aed5cc20280568f6107a
Stef Smeets <s.smeets@esciencecenter.nl>
2021-05-20 11:10:16 +0200

    Add tests for 2d meshing routine

--- tests/test_mesh_2d.py
+++ tests/test_mesh_2d.py
@@ -3,9 +3,20 @@ from pathlib import Path
 
 import helpers
 import numpy as np
+import pytest
 from matplotlib.testing.decorators import image_comparison
 
-from nanomesh.mesh2d import generate_2d_mesh
+from nanomesh.mesh2d import (add_edge_points, add_speckle_grid,
+                             add_speckle_kmeans, generate_2d_mesh,
+                             subdivide_contour)
+
+
+@pytest.fixture
+def segmented():
+    """Segmented binary numpy array."""
+    image_fn = Path(__file__).parent / 'segmented.npy'
+    image = np.load(image_fn)
+    return image
 
 
 @image_comparison(
@@ -14,13 +25,12 @@ from nanomesh.mesh2d import generate_2d_mesh
     extensions=['png'],
     savefig_kwarg={'bbox_inches': 'tight'},
 )
-def test_generate_2d_mesh():
+def test_generate_2d_mesh(segmented):
     """Test 2D mesh generation and plot."""
     expected_fn = Path(__file__).parent / 'segmented_mesh.pickle'
-    image_fn = Path(__file__).parent / 'segmented.npy'
-    image = np.load(image_fn)
 
-    mesh = generate_2d_mesh(image, pad=True, plot=True)
+    np.random.seed(1234)  # set seed for reproducible clustering
+    mesh = generate_2d_mesh(segmented, pad=True, speckle=True, plot=True)
 
     if expected_fn.exists():
         with open(expected_fn, 'rb') as f:
@@ -32,3 +42,59 @@ def test_generate_2d_mesh():
         raise RuntimeError(f'Wrote expected mesh to {expected_fn.absolute()}')
 
     helpers.assert_mesh_almost_equal(mesh, expected_mesh)
+
+
+def test_add_edge_points():
+    """Test generation of edge points."""
+    image = np.zeros((10, 10))
+    image[:5, :5] = 1
+    image[-5:, -5:] = 1
+
+    ret = add_edge_points(image, n_points=5)
+
+    expected = np.array([[0, 0], [2, 0], [4, 0], [6, 9], [9, 9], [0, 2],
+                         [0, 4], [9, 6]])
+
+    assert np.all(ret == expected)
+
+
+def test_add_speckle_grid():
+    """Test grid method for adding points."""
+    image = np.zeros((10, 10))
+    image[:5, :5] = 1
+    image[-5:, -5:] = 1
+
+    ret = add_speckle_grid(image, border=1, n_points=4)
+
+    expected = np.array([[0, 0], [3, 0], [0, 3], [3, 3], [6, 6], [9, 6],
+                         [6, 9], [9, 9]])
+
+    assert np.all(ret == expected)
+
+
+def test_add_speckle_kmeans():
+    """Test kmeans method for adding points."""
+    image = np.zeros((10, 10))
+    image[:5, :5] = 1
+    image[-5:, -5:] = 1
+
+    np.random.seed(9)
+    ret = add_speckle_kmeans(image, iters=5, n_points=10)
+
+    expected = np.array([[3.6, 2.8], [8., 6.], [0., 2.], [2., 3.75], [8., 8.5],
+                         [1.25, 2.], [2.2, 0.4], [4., 0.5], [5.5, 8.],
+                         [5.5, 5.5]])
+
+    np.testing.assert_almost_equal(ret, expected)
+
+
+def test_subdivide_contour():
+    """Test contour subdivision."""
+    contour = np.array([[0, 0], [0, 6], [2, 6], [2, 0], [0, 0]])
+
+    ret = subdivide_contour(contour, max_dist=2)
+
+    expected = np.array([[0., 0.], [0., 2.], [0., 4.], [0., 6.], [2., 6.],
+                         [2., 4.], [2., 2.], [2., 0.], [0., 0.]])
+
+    assert np.all(ret == expected)

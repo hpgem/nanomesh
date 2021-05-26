72384288b184c3b01248c8e8d95025b4712e6e23
Stef Smeets <s.smeets@esciencecenter.nl>
2021-05-26 11:24:49 +0200

    Allow for some tolerance in image comparison on Windows + small refactor

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,13 +1,10 @@
 import numpy as np
 
 
-def assert_mesh_almost_equal(this, other):
+def assert_mesh_almost_equal(this, other, tol=0.0):
     """Compare two meshes."""
-    # There is a minor discrepancy between the generated points between
-    # Linux/Mac and Windows. Allow for some deviation.
-    tolerance = 0.0025
     dist = np.linalg.norm(this.points - other.points, axis=1)
-    assert dist.mean() < tolerance
+    assert dist.mean() < tol
 
     this_cells = this.cells_dict
     other_cells = other.cells_dict

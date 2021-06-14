bfeba3ca34d7dca67dd18d2c160af6ab60d94a8e
Stef Smeets <s.smeets@esciencecenter.nl>
2021-06-14 14:58:13 +0200

    Add logging to stdout

--- nanomesh/__init__.py
+++ nanomesh/__init__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """Documentation about nanomesh."""
 import logging
+import sys
 
 from .__version__ import __version__
 from .plane import Plane
 from .volume import Volume
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.basicConfig(format='%(message)s',
+                    level=logging.INFO,
+                    stream=sys.stdout)
 
 __author__ = 'Nicolas Renaud'
 __email__ = 'n.renaud@esciencecenter.nl'

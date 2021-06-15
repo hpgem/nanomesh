5ce7fc9daf5d82f3ba5ce38006a4793367fc93b2
Stef Smeets <s.smeets@esciencecenter.nl>
2021-06-15 14:48:38 +0200

    Implement 2d meshing using Mesh2D class (like Mesh3D)

--- nanomesh/mesh_utils.py
+++ nanomesh/mesh_utils.py
@@ -8,6 +8,44 @@ import trimesh
 
 
 @dataclass
+class TwoDMeshContainer:
+    vertices: np.ndarray
+    faces: np.ndarray
+
+    def to_trimesh(self) -> 'trimesh.Trimesh':
+        """Return instance of `trimesh.Trimesh`."""
+        return trimesh.Trimesh(vertices=self.vertices, faces=self.faces)
+
+    def to_meshio(self) -> 'meshio.Mesh':
+        """Return instance of `meshio.Mesh`."""
+        cells = [
+            ('triangle', self.faces),
+        ]
+
+        mesh = meshio.Mesh(self.vertices, cells)
+        return mesh
+
+    def to_open3d(self) -> 'open3d.geometry.TriangleMesh':
+        """Return instance of `open3d.geometry.TriangleMesh`."""
+        import open3d
+        return open3d.geometry.TriangleMesh(
+            vertices=open3d.utility.Vector3dVector(self.vertices),
+            triangles=open3d.utility.Vector3iVector(self.faces))
+
+    @classmethod
+    def from_open3d(cls, mesh: 'open3d.geometry.TriangleMesh'):
+        """Return instance of `SurfaceMeshContainer` from open3d."""
+        vertices = np.asarray(mesh.vertices)
+        faces = np.asarray(mesh.triangles)
+        return cls(vertices=vertices, faces=faces)
+
+    @classmethod
+    def from_trimesh(cls, mesh: 'trimesh.Trimesh'):
+        """Return instance of `SurfaceMeshContainer` from open3d."""
+        return cls(vertices=mesh.vertices, faces=mesh.faces)
+
+
+@dataclass
 class SurfaceMeshContainer:
     vertices: np.ndarray
     faces: np.ndarray
@@ -19,7 +57,7 @@ class SurfaceMeshContainer:
     def to_meshio(self) -> 'meshio.Mesh':
         """Return instance of `meshio.Mesh`."""
         cells = [
-            ('tetra', self.faces),
+            ('triangle', self.faces),
         ]
 
         mesh = meshio.Mesh(self.vertices, cells)

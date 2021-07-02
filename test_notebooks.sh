3e92fae589c9ebd9da66266972d76325c931c842
Stef Smeets <s.smeets@esciencecenter.nl>
2021-07-02 12:11:16 +0200

    Add scripts for running all notebooks

---
+++ test_notebooks.sh
@@ -0,0 +1,13 @@
+pytest --no-cov --nbmake \
+   "./notebooks/full_periodic/1domain/generator.ipynb" \
+   "./notebooks/full_periodic/2domain/generator.ipynb" \
+   "./notebooks/non_periodic/1domain/generator_conform_to_boundaries.ipynb" \
+   "./notebooks/non_periodic/2domain/generator_conform_to_boundaries.ipynb" \
+   "./notebooks/semi_periodic/1domain/generator.ipynb" \
+   "./notebooks/semi_periodic/2domain/generator.ipynb" \
+   "./notebooks/sample_data/process_raw_data.ipynb" \
+   "./notebooks/sample_data/multi-domain-plane.ipynb" \
+   "./notebooks/sample_data/sample_data.ipynb" \
+   "./notebooks/sample_data/sample_data_2D.ipynb" \
+   "./notebooks/sample_data/sample_data_3D.ipynb" \
+   "./notebooks/sample_data/select_roi_2d.ipynb " \

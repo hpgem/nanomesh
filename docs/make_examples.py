b4eb15b1706665d0a0287198257bf71d995022b4
Stef Smeets <s.smeets@esciencecenter.nl>
2021-07-09 12:22:40 +0200

    Improve script to generate examples

--- docs/make_examples.py
+++ docs/make_examples.py
@@ -2,16 +2,58 @@ import sys
 from pathlib import Path
 
 import nbformat
+from jinja2 import Template
 from nbconvert import HTMLExporter, RSTExporter
 
 SUBDIR = Path('examples')
 OUTDIR = Path(__file__).parent / '_static' / SUBDIR
-OUTDIR.mkdir(exist_ok=True)
+OUTDIR.mkdir(exist_ok=True, parents=True)
 
 exporters = {
     '.rst': RSTExporter(),
     '.html': HTMLExporter(),
 }
+
+
+class NotebookObject:
+    def __init__(self, path, notebook):
+        self.path = path
+        self.notebook = notebook
+
+    @property
+    def body_text(self):
+        """Grabs the first markdown cell and renders output as `.rst`."""
+        for cell in self.notebook.cells:
+            if cell['cell_type'] == 'markdown':
+                break
+
+        return self.cell_to_rst(cell)
+
+    @property
+    def title(self):
+        title = self.name
+        underline = '=' * len(title)
+        return f'{title}\n{underline}'
+
+    @property
+    def name(self):
+        return self.path.stem
+
+    @property
+    def link(self):
+        return f'examples.{self.path.name}'
+
+    def cell_to_rst(self, cell):
+        """Convert single cell to rst."""
+        new = nbformat.v4.new_notebook(
+            cells=[cell],
+            metadata=self.notebook['metadata'],
+            nbformat=self.notebook['nbformat'],
+            nbformat_minor=self.notebook['nbformat_minor'])
+        exporter = exporters['.rst']
+        (body, resources) = exporter.from_notebook_node(new)
+
+        return body
 
 
 def notebook_as(path, ext):
@@ -26,7 +68,7 @@ def notebook_as(path, ext):
     with open(outfile, 'w') as f:
         f.write(body)
 
-    return outfile
+    return NotebookObject(outfile, notebook)
 
 
 def notebook2html(path):
@@ -37,48 +79,52 @@ def notebook2rst(path):
     return notebook_as(path, '.rst')
 
 
-EXAMPLES_TEMPLATE = """
-Examples
+EXAMPLES_T = Template("""Examples
 ========
 
-"""
+.. toctree::
+   :hidden:
+{% for object in objects %}
+   examples.{{ object.name }}
+{%- endfor %}
 
-NOTEBOOK_TEMPLATE = """
-{title}
-=======
+{% for object in objects %}
+
+{{ object.body_text }}
+
+See: `{{ object.name }} <{{ object.link }}>`_
+
+{% endfor %}
+""")
+
+NOTEBOOK_T = Template("""{{ object.title }}
 
 .. raw:: html
-   :file: {url}
-"""
+   :file: {{ object.path }}
+""")
 
 
 def main():
-    examples_rst = EXAMPLES_TEMPLATE
-
     filenames = (Path(filename) for filename in sys.argv[1:])
+
+    objects = []
 
     for filename in filenames:
         # notebook2rst(filename)
-        html_path = notebook2html(filename)
+        nbobj = notebook2html(filename)
 
-        loc_html = SUBDIR / html_path.name
-
-        title = loc_html.stem
-        examples_rst += f'{title}\n'
-        examples_rst += '-' * len(title) + '\n'
-        examples_rst += f'`{loc_html.name} <{loc_html}>`_\n'
-        examples_rst += '\n'
-
-        loc_rst = f'examples.{html_path.stem}.rst'
+        objects.append(nbobj)
+        loc_rst = f'examples.{nbobj.path.stem}.rst'
 
         with open(loc_rst, 'w') as f:
-            s = NOTEBOOK_TEMPLATE.format(title=title, url=html_path)
+            s = NOTEBOOK_T.render(object=nbobj)
             f.write(s)
 
-        print('Writing', loc_rst, 'embedding', html_path)
+        print('Writing', loc_rst, 'embedding', nbobj.path)
 
     with open('examples.rst', 'w') as f:
-        f.write(examples_rst)
+        examples = EXAMPLES_T.render(objects=objects)
+        f.writelines(examples)
 
 
 if __name__ == '__main__':

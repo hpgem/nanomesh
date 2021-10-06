b4958b09f9f252febeb47e1c646c40c88faa9dcd
Stef Smeets <s.smeets@esciencecenter.nl>
2021-10-06 09:25:48 +0200

    Update and move notebook testing scripts

---
+++ notebooks/test_notebooks.sh
@@ -0,0 +1,18 @@
+pytest --no-cov --nbmake \
+   "./full_periodic/1domain/generator.ipynb" \
+   "./full_periodic/2domain/generator.ipynb" \
+   "./non_periodic/1domain/generator_conform_to_boundaries.ipynb" \
+   "./non_periodic/2domain/generator_conform_to_boundaries.ipynb" \
+   "./semi_periodic/1domain/generator.ipynb" \
+   "./semi_periodic/2domain/generator.ipynb" \
+   "./banner/Make banner.ipynb" \
+   "./sample_data/How to calculate cell quality metrics (triangle mesh).ipynb" \
+   "./sample_data/How to do 3D multi-domain meshing from simulated data volume.ipynb" \
+   "./sample_data/How to examine nanopore properties using regionprops.ipynb" \
+   "./sample_data/How to generate a tetrahedral mesh from a 3D image.ipynb" \
+   "./sample_data/How to generate a triangle mesh from a 2D image.ipynb" \
+   "./sample_data/How to pad a 2D mesh.ipynb" \
+   "./sample_data/How to process a large data file.ipynb" \
+   "./sample_data/How to segment image data using local thresholds.ipynb" \
+   "./sample_data/How to select a region of interest from an image.ipynb" \
+   "./sample_data/How to select a region of interest using FFTs.ipynb \

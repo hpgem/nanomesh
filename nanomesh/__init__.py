51cdfff1eb896afdf63a4f5c705a1bbc4f1c71d0
Stef Smeets <s.smeets@esciencecenter.nl>
2021-10-12 14:33:45 +0200

    Bump version to 0.2.0

--- nanomesh/__init__.py
+++ nanomesh/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(message)s',
 
 __author__ = 'Nicolas Renaud'
 __email__ = 'n.renaud@esciencecenter.nl'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 __all__ = [
     '__author__',

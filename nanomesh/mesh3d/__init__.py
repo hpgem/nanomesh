24e2596036a816dd4135a367373f44eb37f8a9dd
Stef Smeets <s.smeets@esciencecenter.nl>
2021-11-11 13:53:13 +0100

    Fix notebooks

--- nanomesh/mesh3d/__init__.py
+++ nanomesh/mesh3d/__init__.py
@@ -1,6 +1,6 @@
 from .bounding_box import BoundingBox
 from .helpers import pad
-from .mesher import Mesher3D, generate_3d_mesh
+from .mesher import Mesher3D, generate_3d_mesh, get_region_markers
 
 __all__ = [
     'BoundingBox',

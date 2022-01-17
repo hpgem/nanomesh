dbcfa5a982cb5c232d9383a75b68c1b72e9ca710
Stef Smeets <s.smeets@esciencecenter.nl>
2022-01-17 14:36:53 +0100

    Derive default label correctly with any cell data input

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,17 @@ from matplotlib.testing.conftest import mpl_test_settings  # noqa
 
 from nanomesh import LineMesh, MeshContainer, TetraMesh, TriangleMesh
 
+KEY = 'labels'
+
 
 @pytest.fixture
 def line_mesh():
     points = np.arange(10).reshape(5, 2)
     cells = np.zeros((5, 3), dtype=int)
-    cell_data = {'labels': np.arange(5)}
+    cell_data = {KEY: np.arange(5)}
 
     mesh = LineMesh.create(cells=cells, points=points, **cell_data)
+    mesh.default_key = KEY
     assert isinstance(mesh, TriangleMesh)
     return mesh
 
@@ -21,9 +24,10 @@ def line_mesh():
 def triangle_mesh_2d():
     points = np.arange(10).reshape(5, 2)
     cells = np.zeros((5, 3), dtype=int)
-    cell_data = {'labels': np.arange(5)}
+    cell_data = {KEY: np.arange(5)}
 
     mesh = TriangleMesh.create(cells=cells, points=points, **cell_data)
+    mesh.default_key = KEY
     assert isinstance(mesh, TriangleMesh)
     return mesh
 
@@ -32,9 +36,10 @@ def triangle_mesh_2d():
 def triangle_mesh_3d():
     points = np.arange(15).reshape(5, 3)
     cells = np.zeros((5, 3), dtype=int)
-    cell_data = {'labels': np.arange(5)}
+    cell_data = {KEY: np.arange(5)}
 
     mesh = TriangleMesh.create(cells=cells, points=points, **cell_data)
+    mesh.default_key = KEY
     assert isinstance(mesh, TriangleMesh)
     return mesh
 
@@ -43,7 +48,7 @@ def triangle_mesh_3d():
 def tetra_mesh():
     points = np.arange(15).reshape(5, 3)
     cells = np.zeros((5, 4), dtype=int)
-    cell_data = {'labels': np.arange(5)}
+    cell_data = {KEY: np.arange(5)}
 
     mesh = TetraMesh.create(cells=cells, points=points, **cell_data)
     assert isinstance(mesh, TetraMesh)

691569e6e1cc16014501aae37a2f131f510e5a9c
Stef Smeets <stefsmeets@users.noreply.github.com>
2022-01-28 10:18:58 +0100

    Move `simple_triangulate` to own submodule (#193)

--- nanomesh/mesh2d/__init__.py
+++ nanomesh/mesh2d/__init__.py
@@ -1,4 +1,4 @@
-from .helpers import compare_mesh_with_image, pad, simple_triangulate
+from .helpers import compare_mesh_with_image, pad
 from .mesher import Mesher2D, generate_2d_mesh
 
 __all__ = [
@@ -6,5 +6,4 @@ __all__ = [
     'generate_2d_mesh',
     'Mesher2D',
     'pad',
-    'simple_triangulate',
 ]

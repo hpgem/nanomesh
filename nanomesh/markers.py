112551e0f22a17844d07bb18a1c2409e3718ac9d
Stef Smeets <s.smeets@esciencecenter.nl>
2022-01-31 16:51:51 +0100

    Implement SegmentMarker dataclass

--- nanomesh/markers.py
+++ nanomesh/markers.py
@@ -23,3 +23,17 @@ class RegionMarker:
 
 RegionMarkerLike = Union[RegionMarker, Tuple[int, Tuple[float, ...],
                                              Optional[str]]]
+
+
+@dataclass
+class SegmentMarker:
+    label: int
+    point: Union[Tuple[float, ...], npt.NDArray]
+    name: Optional[str] = None
+
+    def __post_init__(self):
+        self.point = tuple(self.point)
+
+
+SegmentMarkerLike = Union[SegmentMarker, Tuple[int, Tuple[float, ...],
+                                               Optional[str]]]

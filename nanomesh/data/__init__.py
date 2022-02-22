9baab9b9eff4d1e6c6f6ffc91fc7a2d093213f3e
Stef Smeets <s.smeets@esciencecenter.nl>
2022-02-22 11:04:06 +0100

    Update functions in data module

--- nanomesh/data/__init__.py
+++ nanomesh/data/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import numpy as np
-from skimage.data import binary_blobs, cells3d, coins, horse
+from skimage.data import binary_blobs
 
 data_dir = Path(__file__).parent
 
@@ -28,11 +28,41 @@ def nanopores3d() -> np.ndarray:
     return np.load(data_dir / 'nanopores3d.npy')
 
 
+def binary_blobs3d(depth: int = 20, **kwargs):
+    """Generate synthetic pores based on :func:`skimage.data.binary_blobs`.
+
+    Parameters
+    ----------
+    depth : int, optional
+        Number of pixels along z-direction.
+    **kwargs
+        These parameters are passed to :func:`skimage.data.binary_blobs`.
+
+    Returns
+    -------
+    numpy.array
+        3D binary pores.
+    """
+    blobs = binary_blobs(**kwargs)
+    return np.tile(blobs, (depth, 1, 1))
+
+
+def triangle_mesh():
+    raise NotImplementedError
+
+
+def triangle_mesh3d():
+    raise NotImplementedError
+
+
+def tetra_mesh():
+    raise NotImplementedError
+
+
 __all__ = [
     'nanopores',
     'nanopores3d',
-    'cells3d',
-    'horse',
-    'binary_blobs',
-    'coins',
+    'binary_blobs3d',
+    'triangle_mesh',
+    'triangle_mesh3d',
 ]

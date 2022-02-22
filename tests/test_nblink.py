a7704687a453f9ef9965cc90866a7300025fbe07
Stef Smeets <s.smeets@esciencecenter.nl>
2022-02-22 16:46:02 +0100

    Update nblink files [WIP]

--- tests/test_nblink.py
+++ tests/test_nblink.py
@@ -1,17 +1,20 @@
 import json
 from pathlib import Path
 
+import pytest
 
-def test_check_if_nblink_complete():
+example_dir = Path(__file__).parents[1] / 'docs' / 'examples'
+nblink_files = example_dir.glob('*.nblink')
+
+
+@pytest.mark.parametrize('file', nblink_files)
+def test_check_if_nblink_complete(file):
     """Test if nblink files point to an existing notebook."""
-    example_dir = Path(__file__).parents[1] / 'docs' / 'examples'
+    with open(file, 'r') as f:
+        data = json.load(f)
 
-    nblink_files = example_dir.glob('*.nblink')
+    path = Path(data['path'])
+    notebook = (file.parent / path)
 
-    for file in nblink_files:
-        with open(file, 'r') as f:
-            data = json.load(f)
-
-        path = Path(data['path'])
-
-        assert (file.parent / path).exists()
+    assert notebook.exists(
+    ), f'{file.name!r} references missing {notebook.name!r}'

1ef9179a5f5739af9f7b35b9485f6cb64ae1c934
Stef Smeets <s.smeets@esciencecenter.nl>
2022-02-22 14:09:47 +0100

    Update 3D multi-domain notebook using blobs

--- nanomesh/data/__init__.py
+++ nanomesh/data/__init__.py
@@ -3,14 +3,48 @@ from pathlib import Path
 import numpy as np
 from skimage.data import binary_blobs
 
+from nanomesh._doc import doc
+
 data_dir = Path(__file__).parent
+
+
+@doc(dim='2d')
+def binary_blobs2d(**kwargs):
+    """Generate {dim} binary blobs.
+
+    Parameters
+    ----------
+    Parameters
+    ----------
+    **kwargs
+        These parameters are passed to :func:`skimage.data.binary_blobs`
+
+    Returns
+    -------
+    numpy.array
+        {dim} array with binary blobs
+    """
+    kwargs.setdefault('length', 50)
+    kwargs.setdefault('n_dim', 2)
+    kwargs.setdefault('volume_fraction', 0.2)
+    kwargs.setdefault('blob_size_fraction', 0.2)
+    return binary_blobs(**kwargs).astype(int)
+
+
+@doc(dim='3d')
+def binary_blobs3d(**kwargs):
+    kwargs.setdefault('length', 50)
+    kwargs.setdefault('n_dim', 3)
+    kwargs.setdefault('volume_fraction', 0.2)
+    kwargs.setdefault('blob_size_fraction', 0.2)
+    return binary_blobs(**kwargs).astype(int)
 
 
 def nanopores() -> np.ndarray:
     """Fetch 2D slice of nanopore dataset.
 
-        Returns
-        -------
+    Returns
+    -------
     nanopores : (i,j) np.ndarray
         2D image of nanopores
     """
@@ -22,40 +56,19 @@ def nanopores3d() -> np.ndarray:
 
     Returns
     -------
-    nanopores 3d : (i,j,k) np.ndarray
-            3D image of nanopores
+    nanopores : (i,j,k) np.ndarray
+        3D image of nanopores
     """
     return np.load(data_dir / 'nanopores3d.npy')
 
 
-def binary_blobs3d(depth: int = 20, **kwargs):
-    """Generate synthetic pores based on :func:`skimage.data.binary_blobs`.
-
-    Parameters
-    ----------
-    depth : int, optional
-        Number of pixels along z-direction.
-    **kwargs
-        These parameters are passed to :func:`skimage.data.binary_blobs`.
-
-    Returns
-    -------
-    numpy.array
-        3D binary pores.
-    """
-    blobs = binary_blobs(**kwargs)
-    return np.tile(blobs, (depth, 1, 1))
-
-
-def triangle_mesh():
+def mesh():
+    """Return a 2d mesh."""
     raise NotImplementedError
 
 
-def triangle_mesh3d():
-    raise NotImplementedError
-
-
-def tetra_mesh():
+def mesh3d():
+    """Return a 3d mesh."""
     raise NotImplementedError
 
 

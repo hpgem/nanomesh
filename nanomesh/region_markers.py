82f963718171f859c6ccdaa2aee0e8dd1b41c7b4
Stef Smeets <s.smeets@esciencecenter.nl>
2022-02-25 14:57:09 +0100

    Add container for region markers

--- nanomesh/region_markers.py
+++ nanomesh/region_markers.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
+from collections import abc
+from copy import copy
 from dataclasses import dataclass
-from typing import Optional, Tuple, Union
+from functools import singledispatchmethod
+from typing import List, Optional, Tuple, Union
 
 import numpy.typing as npt
 
@@ -37,6 +42,76 @@ class RegionMarker:
     def __post_init__(self):
         self.point = tuple(self.point)
 
+    def update(self, **kwargs) -> RegionMarker:
+        new_marker = copy(self)
+        new_marker.__dict__.update(kwargs)
+        return new_marker
+
 
 RegionMarkerLike = Union[RegionMarker, Tuple[int, Tuple[float, ...],
                                              Optional[str]]]
+
+
+class RegionMarkerList(List[RegionMarker]):
+    """List of region markers."""
+
+    def __repr__(self):
+        s = ',\n'.join(f'    {marker}' for marker in self)
+        return f'{self.__class__.__name__}(\n{s}\n)'
+
+    @singledispatchmethod
+    def relabel(self, old: abc.Callable, new: int):
+        markers = (m.update(label=new) if old(m.label) else m for m in self)
+        return RegionMarkerList(markers)
+
+    @relabel.register
+    def _(self, old: list, new: int):
+
+        def f(x):
+            return x in old
+
+        return self.relabel(f, new)
+
+    @relabel.register
+    def _(self, old: int, new: int):
+
+        def f(x):
+            return x == old
+
+        return self.relabel(f, new)
+
+    @singledispatchmethod
+    def label_sequentially(self, old: abc.Callable):
+        markers = [copy(marker) for marker in self]
+        i = max(self.labels) + 1
+        for marker in markers:
+            if old(marker.label):
+                marker.label = i
+                i += 1
+        return RegionMarkerList(markers)
+
+    @label_sequentially.register
+    def _(self, old: list):
+
+        def f(x):
+            return x in old
+
+        return self.label_sequentially(f)
+
+    @property
+    def labels(self) -> set:
+        return set(m.label for m in self)
+
+    def rename(self, old, new):
+        pass
+
+
+if __name__ == '__main__':
+    k = [RegionMarker(x, point=(x, x)) for x in range(10)]
+    m = RegionMarkerList(k)
+
+    new1 = m.relabel(1, 11)
+    new2 = m.relabel([2, 3, 4], 11)
+    new3 = m.relabel(lambda x: x < 4, 11)
+
+    new4 = new3.label_sequentially(lambda x: x == 11)
